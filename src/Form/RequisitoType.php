1725cd59db2ee887c617f088267cb65df1ee74b5
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-04-27 07:15:14 +0200

    Modificado TextType a TextAreaType

--- src/Form/RequisitoType.php
+++ src/Form/RequisitoType.php
@@ -6,6 +6,7 @@ use App\Entity\Grupo;
 use App\Entity\Requisito;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class RequisitoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('descripcion', TextType::class, [
+            ->add('descripcion', TextareaType::class, [
                 'label' => 'Descripción: '
             ])
             ->add('grupo', EntityType::class, [
